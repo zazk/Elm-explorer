e7b3209f08fa8ff2f7418faed5ae72e937dc9905
zazk <juandedioz@gmail.com>
2017-09-10 14:40:22 -0500

    Added Mouse Interaction

---
+++ Messages.elm
@@ -0,0 +1,73 @@
+module Messages exposing (..)
+import Html exposing (Html, button, div, text, program)
+import Html.Events exposing (onClick)
+
+
+-- MODEL
+
+
+type alias Model =
+    Int
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
+
+
+
+-- MESSAGES
+
+
+type Msg
+    = Increment Int
+
+type MsgDec
+    = Decrement Int
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+      [ button [ onClick( Increment 2 ) ] [ text "+" ]
+      , text (toString model)
+      , button [ onClick( Increment 2 ) ] [ text "-" ]
+      ]
+
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+      Increment howMuch ->
+        (model + howMuch, Cmd.none)
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- MAIN
+
+
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
